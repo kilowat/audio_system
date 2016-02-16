7a7c7f7f33d33e441b5bec9d97067978d2c937cb
kilowat <kadantsev.sergey@gmail.com>
2016-02-16 13:49:05 +0400

    fix ie8

--- js/app.js
+++ js/app.js
@@ -2,12 +2,11 @@ _app = function(){
 	return {
 		sliderResize:function(){
 			var sHeadW = $('.slide-head').width();
-			var w = window.innerWidth;
+			var w = $(document).width();
 			var wCenter = $('.header .wrapper').width()/2;
 			var pos = (w/2-wCenter);
 			$('.swiper-pagination').css('left',wCenter+'px');
 			$('.slide-head').css('left',pos+'px');
-		
 		}
 
 	}
