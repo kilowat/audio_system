754abd8f2d18a4a9eaf239779ddea470808cb6b1
kilowat <kadantsev.sergey@gmail.com>
2016-02-16 12:16:52 +0400

    init

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ _app = function(){
 		sliderResize:function(){
 			var sHeadW = $('.slide-head').width();
 			var w = window.innerWidth;
-			var wCenter = $('header .wrapper').width()/2;
+			var wCenter = $('.header .wrapper').width()/2;
 			var pos = (w/2-wCenter);
 			$('.swiper-pagination').css('left',wCenter+'px');
 			$('.slide-head').css('left',pos+'px');
